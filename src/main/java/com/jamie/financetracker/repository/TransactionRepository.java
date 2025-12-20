0ae082ff84eb18c461c7a482d0771c20e81b63dc
JamieSteadman <jamie.nune@gmail.com>
2025-12-19 20:42:36 -0500

    Added JpaRepository functionality

---
+++ src/main/java/com/jamie/financetracker/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.jamie.financetracker.repository;
+
+import com.jamie.financetracker.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
